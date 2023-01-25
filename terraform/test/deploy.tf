83fab0b2813ccd619e6f2ef95620722d0a98f376
indhu <indhu@LAPTOP-761FVFV7>
2023-01-25 22:05:50 +0530

    updaing the labels

--- terraform/test/deploy.tf
+++ terraform/test/deploy.tf
@@ -2,7 +2,7 @@ resource "kubernetes_deployment" "deploymentnginx-1" {
   metadata {
     name = "deploymentnginx-1"
     labels = {
-      App = "nginx"
+      App = "nginx1"
     }
   }
 
@@ -10,13 +10,13 @@ resource "kubernetes_deployment" "deploymentnginx-1" {
     replicas = 1
     selector {
       match_labels = {
-        App = "nginx"
+        App = "nginx1"
       }
     }
     template {
       metadata {
         labels = {
-          App = "nginx"
+          App = "nginx1"
         }
       }
       spec {
