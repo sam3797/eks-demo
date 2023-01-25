3838fafb822272b2348c40a11b422cbec1d17de9
indhu <indhu@LAPTOP-761FVFV7>
2023-01-25 21:32:17 +0530

    updating the iam policy to include ec2:describe

--- terraform/eks/iam.tf
+++ terraform/eks/iam.tf
@@ -58,6 +58,11 @@ resource "aws_iam_role" "worker-nodes-role" {
   policy_arn = "arn:aws:iam::aws:policy/ElasticLoadBalancingFullAccess"
   role    = aws_iam_role.worker-nodes-role.name
  }
+
+  resource "aws_iam_role_policy_attachment" "EC2describesubnets" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerServiceRole"
+  role    = aws_iam_role.worker-nodes-role.name
+ }
  
  resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
